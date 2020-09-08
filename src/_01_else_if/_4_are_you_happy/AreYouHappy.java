c97b80c80b173fd0737cc7698c71b074a3f512c0
jacks <jacks@192.168.0.31>
2020-09-08 16:54:46 -0700

    First piece of code on Eclipse instead of the VNC.

---
+++ src/_01_else_if/_4_are_you_happy/AreYouHappy.java
@@ -0,0 +1,41 @@
+package _01_else_if._4_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String happy = JOptionPane.showInputDialog("Are you happy?");
+		if (happy.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you are doing.");
+		}
+		if (happy.equalsIgnoreCase("no")) {
+			happy = JOptionPane.showInputDialog("Do you want to be happy?");
+		if (happy.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "Change something.");
+		}
+		if (happy.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you are doing.");
+		}
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ src/_01_else_if/_5_own_adventure/IfownAdventure.java
@@ -0,0 +1,42 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class IfownAdventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String story = JOptionPane.showInputDialog("This a story about pootis. You are in the woods and you are trying to find the one and only pootis." + "\n" +  "If you find him, speak to him and tell him what you want to achieve your dreams and he will give it to you as long as you are polite." + "\n" 
+		+ "If you disrespect the pootis, he will get angry and start creating a curse on you." + "\n" + "So, let's begin. Do you want to go towards the left or right passage?"); 
+		if (story.equalsIgnoreCase("left")) {
+			JOptionPane.showMessageDialog(null, "Great, you made it to the shortcut! Now your adventure will be shorter!");
+		
+		}
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
